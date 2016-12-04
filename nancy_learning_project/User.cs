ee9979c5c308a970c04703e74dcc1cc0e641c8f2
Taylan Karaman <taylan.karaman@carsales.com.au>
2016-12-04 17:58:35 +1100

    In DB works

---
+++ nancy_learning_project/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nancy_learning_project
+{
+    public class User
+    {
+        string UserName { get; set; }
+    }
+}
